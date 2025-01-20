e766a7ffe768bcdc6a43a7f96e5348ca11a9fce4
Kevivin Rojas <Kevinrojaszhuma@icloud.com>
2025-01-20 14:47:08 +0100

    Install Axios

--- frontend-mygram/src/App.jsx
+++ frontend-mygram/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,30 @@ function App() {
 }
 
 export default App
+*/
+
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/hello/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the message!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
